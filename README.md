26d86d2ea7c7cbb8bb76fd0ab89b102d14627695
ggasconn <guillermo.gasconsmr@gmail.com>
2019-10-20 13:55:40 +0200

    Implementadas todas la funciones y añadidas tareas al README

--- README.md
+++ README.md
@@ -9,24 +9,29 @@ Repositorio para tener controlado por versiones y actualizado el código del eje
 ## Lista de pendientes por versión
 
 ### Primera versión
-- [ ] Completar documentación funciones
+- [x] Completar documentación funciones
 - [x] Implementar plantilla con funciones y prototipos
 - [x] Bucle en ``` main```
 - [x] Función ```mostrarMenu```
-- [ ] Función ```fichaToStr```
-- [ ] Función ```mostrarTablero```
+- [x] Función ```fichaToStr```
+- [x] Función ```mostrarTablero```
 - [x] Función ```aleat```
 - [x] Función ```toStr```
-- [ ] Función ```puedePonerIzq```
-- [ ] Función ```puedePonerDer```
-- [ ] Función ```ponerFichaIzq``` 
-- [ ] Función ```ponerFichaDer```
+- [x] Función ```puedePonerIzq```
+- [x] Función ```puedePonerDer```
+- [x] Función ```ponerFichaIzq``` 
+- [x] Función ```ponerFichaDer```
 - [x] Función ```salvarPartida```
 - [x] Función ```comprobarPartida```
 - [x] Función ```recuperarPartida```
+- [ ] Implementar opción robar ficha
+- [ ] Generación de fichas durante la partida
+- [ ] Aumento de contadores durante la partida
+- [ ] Salvar contadores a fichero al guardar
+- [ ] Recuperar contadores a fichero al guardar
 
 #### Errores a solventar
-- [ ] La función ```recuperarPartida``` devuelve un uno antes de la cadena
+- [ ] La función ```recuperarPartida``` devuelve un uno antes de la cadena. *EDIT: compilando y ejecutando el Linux parece que deja de aparecer ese 1.
 
 ### Segunda versión
 ### Tercera versión
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 #include <iostream>
 #include <cstdlib>
-#import <time.h>
+#include <time.h>
 #include <string>
 #include <stdexcept>  //Contiene metodos que permiten devolver excepciones
 #include <fstream>
@@ -22,8 +22,8 @@ short int aleat();
 string toStr(int n); // Se puede usar la implementada en C++11??
 bool puedePonerIzq(string tablero, short int fichaN1, short int fichaN2);
 bool puedePonerDer(string tablero, short int fichaN1, short int fichaN2);
-int ponerFichaIzq(string tablero, short int fichaN1, short int fichaN2);
-int ponerFichaDer(string tablero, short int fichaN1, short int fichaN2);
+string ponerFichaIzq(string tablero, short int fichaN1, short int fichaN2);
+string ponerFichaDer(string tablero, short int fichaN1, short int fichaN2);
 bool salvarPartida(string tablero);
 char comprobarPartida();
 string recuperarPartida();
@@ -34,7 +34,15 @@ string recuperarPartida();
 */
 int main() {
     short int opcionElegida;
-    string tablero = "";
+    short int numColocadas;
+    short int numRobadas;
+    short int fichaN1 = 6;
+    short int fichaN2 = 4;
+    bool haRobado;
+    string tablero = "|6-4|";
+
+    //Mostrar tablero
+    mostrarTablero(6, 6, tablero, 4, 2);
 
     do {
         opcionElegida = mostrarMenu();
@@ -42,11 +50,11 @@ int main() {
         switch (opcionElegida)
         {
         case 1:
-            cout << "Elegida opción 1" << endl;
+            tablero = ponerFichaIzq(tablero, fichaN1, fichaN2);
             break;
         
         case 2:
-            cout << "Elegida opción 2" << endl;
+            tablero = ponerFichaDer(tablero, fichaN1, fichaN2);
             break;
         
         case 3:
@@ -69,12 +77,17 @@ int main() {
             break;
         }
 
+        //Mostrar tablero
+        mostrarTablero(6, 6, tablero, 4, 2);
+
     } while(opcionElegida != 0);
 }
 
 
 /**
 * Devuelve menu principal con opciones y recupera la elegida.
+*
+* @return Devuelve un entero, que hace referencia a la opcion elegida.
 */
 int mostrarMenu() {
     short int opcionElegida; // Almacena lo que el usuario elije del menu
@@ -118,21 +131,32 @@ string fichaToStr(short int izquierda, short int derecha){
 
 
 /**
-* Breve descripcion de la funcion
+* Devuelve el tablero de la partida con los valores recibidos
 *
 * @param fichaN1
 * @param fichaN2
 * @param tablero
 * @param numColocadas
 * @param numRobadas
+*
+* @return La funcion no devuelve nada, pero durante la ejecución
+* va imprimiendo el tablero con los datos recibidos
 */
-void mostrarTablero(short int fichaN1, short int fichaN2, string tablero, int numColocadas, int numRobadas){
-
+void mostrarTablero(short int fichaN1, short int fichaN2, 
+                    string tablero, int numColocadas, int numRobadas){
+    cout << " ------------------" << endl;
+    cout << "|     TABLERO      |" << endl;
+    cout << " ------------------" << endl;
+    cout << tablero << endl;
+    cout << "Fichas colocadas: " << numColocadas << " - Fichas robadas: " << numRobadas << endl;
+    cout << "Ficha jugador: " << fichaToStr(fichaN1, fichaN2) << endl << endl;
 }
 
 
 /**
-* Deuvuelve un entero aleatorio entre 1 y 6
+* Devuelve un entero aleatorio entre 1 y 6
+*
+* @return Devuelve un entero dentro del rango 1-6
 */
 short int aleat(){
     int numAleat;
@@ -146,9 +170,11 @@ short int aleat(){
 
 
 /**
-* Breve descripcion de la funcion
+* Convierte un entero recibido a string
 *
-* @param n
+* @param n Entero a convertir
+*
+* @return Devuelve una cadena con el entero recibido
 */
 string toStr(int n){ // Se puede usar la implementada en C++???
     return to_string(n);
@@ -161,9 +187,17 @@ string toStr(int n){ // Se puede usar la implementada en C++???
 * @param tablero
 * @param fichaN1
 * @param fichaN2
+*
+* @return Devuelve true si se puede poner, false si no.
 */
 bool puedePonerIzq(string tablero, short int fichaN1, short int fichaN2){
+    //Usando indices, saca el segundo caracter de la cadena
+    bool sePuede = false;
+    short int extremoTablero =  int(tablero[1]) - int('0');
 
+    if (fichaN1 == extremoTablero || fichaN2 == extremoTablero) sePuede = true;
+
+    return sePuede;
 }
 
 
@@ -173,33 +207,74 @@ bool puedePonerIzq(string tablero, short int fichaN1, short int fichaN2){
 * @param tablero
 * @param fichaN1
 * @param fichaN2
+*
+* @return Devuelve true si se puede poner, false si no.
 */
 bool puedePonerDer(string tablero, short int fichaN1, short int fichaN2){
+    bool sePuede = false;
+    short int extremoTablero = int(tablero[tablero.size() - 2]) - int('0');
 
+    if (fichaN1 == extremoTablero || fichaN2 == extremoTablero) sePuede = true;
+
+    return sePuede;
 }
 
 
 /**
-* Breve descripcion de la funcion
+* Recibe el tablero y la ficha a colocar, si se puede realizar la acción
+* coloca la ficha en la posicion adecuada dentro del tablero, si no se puede
+* deja el tablero como estaba.
 *
 * @param tablero
 * @param fichaN1
 * @param fichaN2
+*
+* @return Devuelve el tablero actualizado o no.
 */
-int ponerFichaIzq(string tablero, short int fichaN1, short int fichaN2){
+string ponerFichaIzq(string tablero, short int fichaN1, short int fichaN2){
+    short int extremoTablero =  int(tablero[1]) - int('0');
+    string fichaFinal;
 
+    if (puedePonerIzq(tablero, fichaN1, fichaN2)) {
+        if (extremoTablero == fichaN1) {
+            fichaFinal = fichaToStr(fichaN2, fichaN1);
+        }else {
+            fichaFinal = fichaToStr(fichaN1, fichaN2);
+        }
+
+        tablero = tablero.insert(0, fichaFinal);
+    }
+
+    return tablero;
 }
 
 
 /**
-* Breve descripcion de la funcion
+* Recibe el tablero y la ficha a colocar, si se puede realizar la acción
+* coloca la ficha en la posicion adecuada dentro del tablero, si no se puede
+* deja el tablero como estaba.
 *
 * @param tablero
 * @param fichaN1
 * @param fichaN2
+*
+* @return Devuelve el tablero actualizado o no.
 */
-int ponerFichaDer(string tablero, short int fichaN1, short int fichaN2){
+string ponerFichaDer(string tablero, short int fichaN1, short int fichaN2){
+    short int extremoTablero = int(tablero[tablero.size() - 2]) - int('0');
+    string fichaFinal;
 
+    if (puedePonerDer(tablero, fichaN1, fichaN2)) {
+        if (extremoTablero == fichaN1) {
+            fichaFinal = fichaToStr(fichaN1, fichaN2);
+        }else {
+            fichaFinal = fichaToStr(fichaN2, fichaN1);
+        }
+
+        tablero = tablero.append(fichaFinal);
+    }
+
+    return tablero;
 }
 
 
@@ -209,6 +284,8 @@ int ponerFichaDer(string tablero, short int fichaN1, short int fichaN2){
 * guardada y se pregunta si se desea sobreescribir.
 *
 * @param tablero. Contiene el estado actual de la partida.
+*
+* @return Devuelve true si la partida se guardo, false si no.
 */
 bool salvarPartida(string tablero){
     bool partidaSalvada = false;
@@ -270,11 +347,10 @@ string recuperarPartida(){
     if (! ficheroPartida.is_open()) {
         throw runtime_error("No se pudo abrir el fichero con el historial. Compruebe si existe o si ha sido borrado.");
     }else {
-        cout << getline(ficheroPartida, tablero);
+        getline(ficheroPartida, tablero);
     }
 
     ficheroPartida.close();
 
     return tablero;
 }
-
